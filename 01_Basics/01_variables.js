aa50bd37300a79cb130106620454fd3b48445217
Anjan Shrestha <91259829+anjanshrestha8@users.noreply.github.com>
2024-03-18 21:05:09 +0000

    variable code added

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+// Constant Variable
+
+const accountId =12345;
+let accountEmail ="anjan@google.com";
+var accountPassword ="098765";
+accountCity ="Kathmandu";
+let accountState;
+
+// accountId = 2;   Since accountId is const so cannot b changed;
+accountEmail ="shrestha@google.com";
+accountPassword ="00000";
+accountCity ="Pokhara";
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
