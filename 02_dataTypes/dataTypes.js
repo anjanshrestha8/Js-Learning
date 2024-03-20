7b09b3568e187df1e93695e0e463d4a036220ec6
Anjan Shrestha <91259829+anjanshrestha8@users.noreply.github.com>
2024-03-20 20:08:20 +0000

    data types and variable

---
+++ 02_dataTypes/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all JS code as newer version;
+
+// alert(3+3) --> we are using note js not browser 
+
+console.log(3+3);
+console.log();
+
+
+let name = "Anjan Shrestha";
+let age =18;
+let isLoggedin =false;
+
+
+// number ==> to 2power 58
+// string ==>"";
+// boolean ==> true /false
+//  null ==> standalone value
+// undefined ==> 
+// symbols ==> unique
+
+
+// object 
+
+
+console.log(typeof NULL); // OBJECT
+console.log(typeof undefined); // undefined
